48a4d4962c080be800b2bd6da7b372c42eb5399d
16Veronika <14.siana.11@gmail.com>
2024-02-24 18:54:18 +0200

    Рабочий код в 'сыром' виде

--- Program.cs
+++ Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-       
+        Console.WriteLine("Введите элементы массива через пробел: ");
+        string input = Console.ReadLine();
+        string[] inputArray = input.Split(' ');
+        Console.WriteLine();
+
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            Console.Write($"\"{inputArray[i]}\"");
+        }
+
+        string[] newArray = new string[inputArray.Length];
+        int newIndex = 0;
+
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                newArray[newIndex] = inputArray[i];
+                newIndex++;
+            }
+        }
+
+        for (int i = 0; i < newIndex; i++)
+        {
+            Console.Write($"\"{newArray[i]}\"");
+        }
     }
 }
